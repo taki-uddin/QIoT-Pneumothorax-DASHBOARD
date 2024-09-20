837513f2d99c3f9ede52114c596ec6f8579b0520
taki-uddin <taki-uddin@users.noreply.github.com>
2024-09-20 03:06:36 +0000

    deploy: 6ce9afab3a5b3e8b1483b1c03c5045c0a427317a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,12 +33,12 @@ const RESOURCES = {"assets/AssetManifest.json": "599cbc54193afd8074db0008a81a6e7
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "bf6bb9e485c047fbbc2c88797da59f41",
+"flutter_bootstrap.js": "0691ebf40b15f8a0dbdeecc1c017697d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "27c938dd764a8627f4fae143463236b6",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"index.html": "4cc44e4ce14a319501186ba64f01b534",
-"/": "4cc44e4ce14a319501186ba64f01b534",
+"index.html": "6b7d68bf3b30531212837cf47fe16113",
+"/": "6b7d68bf3b30531212837cf47fe16113",
 "version.json": "4159e390d390e7afce2e2f3d86528b0d",
 "manifest.json": "96374c1854067313fd8b16d0fd287df6"};
 // The application shell files that are downloaded before a service worker can
